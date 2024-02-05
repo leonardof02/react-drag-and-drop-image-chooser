a674a272a4d7ae82fd33bfb3efe8f94210aa0fcf
_leodev <84308529+leonardof02@users.noreply.github.com>
2024-02-04 19:18:34 -0500

    feat: Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import DDImageChooser from "@/components/DDImageChooser";
+import { MdImage } from "react-icons/md";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      {/* <DDImageChooser /> */}
+    </main>
+  );
+}
